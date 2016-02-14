2be55a62dc0a6febbf8692a4003d02e7af06194b
Sergey Sova <i.am@lestad.net>
2016-02-14 17:22:42 +0300

    add: norender(Delete button)

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -38,6 +38,16 @@ export default class WeatherCard extends Component {
     dropCity(this.props.city.id);
   }
 
+  renderCloseButton() {
+    return (
+      <button styleName="Delete" onClick={this.onDropClick}>
+        <svg viewBox="0 0 24 24">
+          <path fill="#ffffff" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+        </svg>
+      </button>
+    )
+  }
+
   /**
    * Render component WeatherCard
    */
