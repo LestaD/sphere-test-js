6d7e09c6732da9679140a11268272541deeb5d15
Sergey Sova <i.am@lestad.net>
2016-02-14 04:00:10 +0300

    add: icons utils

---
+++ src/utils/icons.js
@@ -0,0 +1,21 @@
+
+import weatherIcons from 'icons.json';
+
+
+/**
+ * Get icon class for weather
+ * @param  {Object} options.weather Item in forecast
+ * @return {String}                 Class name with prefix
+ */
+export function getIcon({ weather }) {
+  const prefix = 'wi wi-';
+  const code = weather[0].id;
+  let icon = weatherIcons[code].icon;
+
+  // If we are not in the ranges mentioned above, add a day/night prefix.
+  if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+    icon = 'day-' + icon;
+  }
+
+  return prefix + icon;
+}
