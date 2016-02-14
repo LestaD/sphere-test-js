8d5609f224f5d4889a2e0bc55c5fb59e05c2c83f
Sergey Sova <i.am@lestad.net>
2016-02-14 22:10:05 +0300

    add: weather plate

--- src/components/WeatherSelector/WeatherSelector.jsx
+++ src/components/WeatherSelector/WeatherSelector.jsx
@@ -1,4 +1,7 @@
+import CN from 'classnames';
 import css from './WeatherSelector.styl';
+import { wi } from 'weather/weather-icons.css';
+import wic from 'weather/weather-icons-wind.css';
 import React, { Component, PropTypes } from 'react';
 
 const { shape, number, string, arrayOf, bool } = PropTypes;
@@ -39,13 +42,43 @@ export default class WeatherSelector extends Component {
     this.setState({ selected: id });
   }
 
+
+  renderPlate({ wind, main, weather, dt }) {
+    const date = new Date(dt * 1000); // convert to ms
+    const [weekday, month, day, year] = date.toDateString().split(' ');
+
+    return (
+      <div styleName="Main">
+        <div styleName="Info">
+          <div styleName="Weather">
+            <p>{day} {month}</p>
+            <i className={getIcon({ weather })} />
+          </div>
+          <div styleName="Wind">
+            <p>Wind</p>
+            <i className={CN(wi, wic['wi-wind'])} style={{ transform: `rotate(${wind.deg}deg)` }} />
+          </div>
+        </div>
+        <div styleName="Params">
+          <p>Humidity: <b>{main.humidity}%</b></p>
+          <p>Pressure: <b>{main.pressure}mm</b></p>
+          <p>Wind: <b>{wind.speed} m/s</b></p>
+        </div>
+      </div>
+    );
+  }
+
   /**
    * Render component WeatherSelector
    */
   render() {
+    const { list } = this.props;
+    const { selected } = this.state;
+
+
     return (
       <div styleName="WeatherSelector">
-
+        {this.renderPlate(list[selected])}
       </div>
     );
   }
